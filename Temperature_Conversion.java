08dc1b6df4a48e5858e92c67c67241a403925e8a
Lakshman velaga <97463083+21mh1a0564@users.noreply.github.com>
2023-02-01 11:01:34 +0530

    import java.util.*; class frclc {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float c=sc.nextFloat();         float f;         f=(float)(32+(c*9/5));         System.out.format("%.2f",f);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class frclc
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float c=sc.nextFloat();
+        float f;
+        f=(float)(32+(c*9/5));
+        System.out.format("%.2f",f);
+        
+    }
+}
